6d32a1899e31366e82cd61def042a5dbc54fef4e
Vjeran Hanzek <vjeran.hanzek@gmail.com>
2019-06-30 13:41:48 +0200

    Initial Commit

---
+++ BetterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BetterApp
+//
+//  Created by Vjeran Hanzek on 30/06/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
