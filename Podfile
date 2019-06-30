23b15c044e1b78a47a1d43daf4922e5c7be9e936
Vjeran Hanzek <vjeran.hanzek@gmail.com>
2019-06-30 13:58:32 +0200

    Initial commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'BetterApp' do
+  pod 'PureLayout'
+  pod 'Kingfisher'
+end
---
+++ Pods/Target Support Files/Pods-BetterApp/Pods-BetterApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BetterApp : NSObject
+@end
+@implementation PodsDummy_Pods_BetterApp
+@end
